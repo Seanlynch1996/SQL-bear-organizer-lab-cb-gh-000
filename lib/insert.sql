50495e4ce6526fdef7fc231e44ec6843a8d59353
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2017-12-20 03:49:53 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,23 +1,23 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive)
-VALUES (1, "Mr. Chocolate", 1, 'M',"yellow", "happy", true);
+VALUES (1, "Mr. Chocolate", 1, 'M',"yellow", "happy", TRUE);
 
 INSERT INTO bears (id, name, age, gender, color, temperament, alive)
-VALUES (2, "Rowdy", 2, 'M',"brown", "angry", true);
+VALUES (2, "Rowdy", 2, 'M',"brown", "angry", TRUE);
 
 INSERT INTO bears (id, name, age, gender, color, temperament, alive)
-VALUES (3, "Tabitha", 3, 'F',"black", "nada", false);
+VALUES (3, "Tabitha", 3, 'F',"black", "nada", FALSE);
 
 INSERT INTO bears (id, name, age, gender, color, temperament, alive)
-VALUES (4, "Sergeant Brown", 4, 'F',"brown", "nada", false);
+VALUES (4, "Sergeant Brown", 4, 'F',"brown", "nada", FALSE);
 
 INSERT INTO bears (id, name, age, gender, color, temperament, alive)
-VALUES (5, "Melissa", 5, 'F',"black", "hungry", true);
+VALUES (5, "Melissa", 5, 'F',"black", "hungry", TRUE);
 
 INSERT INTO bears (id, name, age, gender, color, temperament, alive)
-VALUES (6, "Grinch", 6, 'M',"yellow", "smart", true);
+VALUES (6, "Grinch", 6, 'M',"yellow", "smart", TRUE);
 
 INSERT INTO bears (id, name, age, gender, color, temperament, alive)
-VALUES (7, "Wendy", 7, 'F',"yellow", "goofy", true);
+VALUES (7, "Wendy", 7, 'F',"yellow", "goofy", TRUE);
 
 INSERT INTO bears (id, age, gender, color, temperament, alive)
-VALUES (8, 8, 'M',"black", "sad", false);
+VALUES (8, 8, 'M',"black", "sad", FALSE);
